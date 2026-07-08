5f9dae837477ff139234061347956f4bb2198ff4
Doloris <dolorishouessou@gmail.com>
2026-07-08 16:28:24 +0100

    Ajout

--- index.js
+++ index.js
@@ -1,44 +1,58 @@
-//Chercher,transformer et acceder aux données du fichier JSON
-let extensions=[];//variable pour stocker les extensions
+//Chercher, transformer et accéder aux données du fichier JSON
+let mesExtensions = []; //variable pour stocker les extensions
+
 fetch('./data.json')
-    .then((res) => res.json())
-    .then((data) => {
-        extensions = data;
-        afficherExtensions(extensions);
-    }); 
-    
+    .then((reponse) => reponse.json())
+    .then((donnees) => {
+        mesExtensions = donnees;
+        afficherExtensions(mesExtensions);
+    });
+
 //Afficher les cases
-function afficherExtensions(liste){
+function afficherExtensions(listeExtensions){
     const grille = document.getElementById("grille");
     grille.innerHTML = "";
-    liste.forEach((item, index) => {
+
+    listeExtensions.forEach((extension, position) => {
         grille.innerHTML += `
         <div class="carte">
             <div class="carte-haut">
-                <img src="${item.logo}" alt="${item.name}" class="carte-icone">
+                <img src="${extension.logo}" alt="${extension.name}" class="carte-icone">
             </div>
             <div>
-                <h3 class="carte-titre">${item.name}</h3>
-                <p class="carte-description">${item.description}</p>    
+                <h3 class="carte-titre">${extension.name}</h3>
+                <p class="carte-description">${extension.description}</p>    
             </div>    
-            
-            
 
             <div class="carte-bas">
                 <button 
                     class="button-remove"
-                    onclick="supprimerExtension(${index})">
+                    onclick="supprimerExtension(${position})">
                     Remove
-
                 </button>
                 <button  
-                    class="toggle ${item.active ? " on": ""}"
-                    onclick="toggleExtension(${index})">
-                    
+                    class="toggle ${extension.isActive ? "on" : ""}"
+                    onclick="toggleExtension(${position})">
                 </button>
             </div>
+        </div>
         `;
     });
+}
+
+// Le toggle : bascule isActive entre true et false
+function toggleExtension(position){
+    if (mesExtensions[position].isActive == true) {
+        mesExtensions[position].isActive = false;
+    } else {
+        mesExtensions[position].isActive = true;
+    }
+    afficherExtensions(mesExtensions);
+}
+// Supprimer une extension
+function supprimerExtension(position){
+    mesExtensions.splice(position, 1);
+    afficherExtensions(mesExtensions);
 }
 
 
